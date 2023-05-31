381850e3b4256bba48c5ab4daecd31586376340f
s0ming <126849595+s0ming@users.noreply.github.com>
2023-05-31 22:07:44 +0900

    5월31일

---
+++ W11/stp1/js/ mouseAction.js
@@ -0,0 +1,9 @@
+let box = document.getElementById("box2");
+console.log(box);
+box.onclick = () => {
+  box.classList.toggle("boxSelected");
+};
+
+// let = 변수
+// => = function
+// onclick = 클릭 했을 때 ~~
